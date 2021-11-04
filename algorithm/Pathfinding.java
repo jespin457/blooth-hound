6c90fd10d6bea03c64cfbd517d3361a300e0016c
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-11-04 18:05:49 -0500

    Working on randomizeStartBorder

--- algorithm/Pathfinding.java
+++ algorithm/Pathfinding.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Pathfinding {
   public static void main(String[] args) {
     System.out.println("Pathfinding.java says: Hello World");
@@ -10,16 +12,22 @@ public class Pathfinding {
 
 class Board {
   String[][] grid;
+  int nOfRows;
+  int nOfCols;
 
   //CLASS CONSTRUCTOR
   public Board(int nRows, int nCols) {
     grid = new String[nRows][nCols];
+    nOfRows = nRows;
+    nOfCols = nCols;
 
     for (String[] row : grid) {
       for (int i = 0; i < row.length; i++) {
         row[i] = "O";
       }
     }
+
+    this.randomizeStartBorder();
   }
 
   //DISPLAY GRID METHOD
@@ -31,6 +39,15 @@ class Board {
       }
       System.out.println("");
     }
+  }
+
+  //RANDOMIZES THE STARTING POINT OF THE PIECE ON A BORDER SPACE
+  //START IS INDICATED BY S
+  private void randomizeStartBorder() {
+    int start_x = (int) (Math.random() * (nOfCols - 1));
+    int start_y = (int) (Math.random() * (nOfRows - 1));
+
+    grid[start_y][start_x] = "S";
   }
 }
 
