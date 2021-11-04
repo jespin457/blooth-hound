c21fc9a11e7ef97a2775979548dec15d5182bd3d
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-11-04 17:11:37 -0500

    Beginning work on the algorithm itself

---
+++ algorithm/Pathfinding.java
@@ -0,0 +1,13 @@
+public class Pathfinding {
+  public static void main(String[] args) {
+    System.out.println("Pathfinding.java says: Hello World");
+  }
+}
+
+class Board {
+
+}
+
+class Piece {
+  
+}
